b098c9f18dc6a43f62c2462e7b69ace9f5828003
gerhardadler <gerhardskartveit@gmail.com>
2023-04-24 11:23:01 +0200

    Moved config to config.example

---
+++ config.example.py
@@ -0,0 +1,3 @@
+# Copy to config.py, and add client id and secret from Spotify app here
+SPOTIFY_CLIENT_ID = ""
+SPOTIFY_CLIENT_SECRET = ""
--- config.py
+++
@@ -1,3 +0,0 @@
-# Add client id and secret from Spotify app here
-SPOTIFY_CLIENT_ID = ""
-SPOTIFY_CLIENT_SECRET = ""
